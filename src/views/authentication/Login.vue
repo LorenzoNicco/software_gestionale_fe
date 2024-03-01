359b7fec2c9c83480f543a9e46b07ec326dd97ed
LorenzoNicco <lorenzoniccolai91@gmail.com>
2024-03-01 16:30:14 +0100

    add registrazione utente

--- src/views/authentication/Login.vue
+++ src/views/authentication/Login.vue
@@ -1,24 +1,45 @@
 <script>
-    import { useRouter } from 'vue-router';
-
 export default {
     data() {
         return {
-            // router: useRouter(),
-            form: {
+            formLogin: { //Campi per il login
                 email: '',
                 password: ''
+            },
+            formRegister: { //Campi per la registrazione
+                name: '',
+                email: '',
+                password: '',
+                password_confirmation: ''
             }
         }
     },
     methods: {
         //Funzione per l'invio del form di login
-        submit() {
+        submitLogin() {
             axios.get("http://localhost:8000/sanctum/csrf-cookie")
             .then(()=>{
                 axios.post("http://localhost:8000/login", {
-                    email: this.form.email,
-                    password: this.form.password
+                    email: this.formLogin.email,
+                    password: this.formLogin.password
+                }).then(()=>{
+                    this.$router.push("/");
+                    axios.get("http://localhost:8000/api/user")
+                    .then((response)=>{
+                        console.log(response.data);
+                    });                    
+                });
+            });
+        },
+        //Funzione per l'invio del form di registrazione
+        submitRegister() {
+            axios.get("http://localhost:8000/sanctum/csrf-cookie")
+            .then(()=>{
+                axios.post("http://localhost:8000/register", {
+                    name: this.formRegister.name,
+                    email: this.formRegister.email,
+                    password: this.formRegister.password,
+                    password_confirmation: this.formRegister.password_confirmation
                 }).then(()=>{
                     this.$router.push("/");
                     axios.get("http://localhost:8000/api/user")
@@ -33,20 +54,44 @@ export default {
 </script>
 
 <template>
-    <h1 class="text-4xl font-bold">Login</h1>
+    <h1 class="text-4xl font-bold">Login o registrati</h1>
 
     <div>
-        <form @submit.prevent="submit" class="login">
+        <form @submit.prevent="submitLogin">
             <div class="form-group">
                 <label>Email</label>
-                <input v-model="form.email" type="text" class="form-input">
+                <input v-model="formLogin.email" type="text" class="form-input">
             </div>
             <div class="form-group">
                 <label>Password</label>
-                <input v-model="form.password" type="password" class="form-input">
+                <input v-model="formLogin.password" type="password" class="form-input">
             </div>
             <div class="form-group">
                 <button type="submit" class="form-input btn">Login</button>
+            </div>
+        </form>
+
+        <hr>
+
+        <form @submit.prevent="submitRegister">
+            <div class="form-group">
+                <label>Nome</label>
+                <input v-model="formRegister.name" type="text" class="form-input">
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input v-model="formRegister.email" type="text" class="form-input">
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input v-model="formRegister.password" type="password" class="form-input">
+            </div>
+            <div class="form-group">
+                <label>Conferma password</label>
+                <input v-model="formRegister.password_confirmation" type="password" class="form-input">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="form-input btn">Registrati</button>
             </div>
         </form>
     </div>
