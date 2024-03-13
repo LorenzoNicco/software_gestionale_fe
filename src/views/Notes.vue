465ea1c76aac00ff5cb2f11ce809cd2ca90c9936
LorenzoNicco <lorenzoniccolai91@gmail.com>
2024-03-13 16:19:02 +0100

    add modifica note

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -8,6 +8,10 @@ export default {
                 title: '',
                 noteBody: ''
             },
+            formUpdateNotes: { //Campi per modificare le note
+                title: '',
+                noteBody: ''
+            },
         }
     },
     methods: {
@@ -39,12 +43,63 @@ export default {
                 this.notesDataRethrieve();
 
                 //Svuoto tutti i campi del form e deil data
-                document.getElementById('new_note_form').reset();
-                this.formStoreNotes.title = '';
-                this.formStoreNotes.noteBody = '';
-
-                console.log('creazione', response.data.notes);
+                this.formEntriesReset(this.formStoreNotes, 'new_note_form');
             });
+        },
+
+        //Funzione per aprire e chiudere la finestra di modifica della nota
+        updateAccordionToggle(targetId) {
+            this.formEntriesReset(this.formUpdateNotes, 'update_note_form');
+
+            //Apro o chiudo il contenitore del form di modifica della nota in questione
+            let updateAccordion = document.getElementById(targetId);
+            updateAccordion.classList.toggle('collapse-close');
+            updateAccordion.classList.toggle('collapse-open');
+        },
+
+        //Funzione per modificare le note
+        noteUpdate(formId, noteId) {
+            axios.put("http://localhost:8000/api/notes/" + noteId, {
+                title: this.formUpdateNotes.title,
+                noteBody: this.formUpdateNotes.noteBody,
+            })
+            .then((response) => {                
+                //Refresho le note esistenti
+                this.notesDataRethrieve();
+
+                //Svuoto tutti i campi del form e del data
+                this.formEntriesReset(this.formUpdateNotes, formId)
+            });
+        },
+
+        //Funzione per svuotare i campi provenienti dai form in data e i form
+        formEntriesReset(dataToReset, formToReset) {
+            //Prendo tutti i form da svuotare
+            const formsToReset = document.getElementsByClassName(formToReset);
+
+            //Svuoto il form
+            for(let i = 0; i < formsToReset.length; i++) {
+                formsToReset[i].reset();
+            }
+
+            //Svuoto i dati provenienti dal form in data
+            for (let singleData in dataToReset) {
+                dataToReset[singleData] = '';
+            }
+        },
+
+        //Funzione che chiude il collapse di modifica di ogni nota all'apertura di un'altra nota
+        closeEveryUpdateCollapse() {
+            //Prendo tutti collapse di modifica
+            const updateCollapseList = document.getElementsByClassName('collapse_update');
+
+            //Tolgo la classe open, se presente, e aggiungo quella close
+            for( let i = 0; i < updateCollapseList.length; i++) {
+                if(updateCollapseList[i].classList.contains('collapse-open')) {
+                    updateCollapseList[i].classList.remove('collapse-open');
+                    updateCollapseList[i].classList.add('collapse-close');
+                }
+            }
         }
     },
     mounted() {
@@ -65,7 +120,7 @@ export default {
             <ul>
                 <!-- Elemento della lista delle note -->
                 <li v-for="singleNote in notesData" class="collapse collapse-arrow shadow-md">
-                    <input type="radio" name="my-accordion-2" /> 
+                    <input type="radio" name="my-accordion-2" v-on:click="closeEveryUpdateCollapse" /> 
 
                     <!-- Titolo della nota -->
                     <div class="collapse-title text-xl font-medium">{{ singleNote.title }}</div>
@@ -76,7 +131,7 @@ export default {
 
                         <div>
                             <!-- Pulsante di modifica nota -->
-                            <button class="btn btn-warning btn-sm btn-square">
+                            <button v-on:click="updateAccordionToggle(singleNote.id)" class="btn btn-warning btn-sm btn-square">
                                 <font-awesome-icon :icon="['fas', 'pen-to-square']" />
                             </button>
                             
@@ -85,6 +140,16 @@ export default {
                                 <font-awesome-icon :icon="['fas', 'trash-can']" />
                             </button>
                         </div>
+                    </div>
+
+                    <div :id="singleNote.id" class="collapse collapse-close collapse_update"> 
+                        <form :id="'update_note_' + singleNote.id" class="collapse-content update_note_form">
+                            <input v-model="formUpdateNotes.title" type="text" placeholder="Nuovo titolo" class="input input-bordered w-full" name="title" minlength="1" maxlength="50" required />
+
+                            <textarea v-model="formUpdateNotes.noteBody" class="textarea textarea-bordered my-4" placeholder="Nuovo testo della nota" name="noteBody" required></textarea>
+
+                            <input v-on:click.prevent="noteUpdate('update_note_' + singleNote.id, singleNote.id)" type="submit" value="Invia" class="btn btn-success">
+                        </form>
                     </div>
                 </li>
             </ul>
@@ -96,12 +161,13 @@ export default {
                 <span>Nuova nota</span> <font-awesome-icon :icon="['fas', 'plus']" />
             </button>
 
+            <!-- Modale di aggiunta nota -->
             <dialog id="insert_note_modal" class="modal">
                 <div class="modal-box">
                     <h3 class="font-bold text-lg">Inserisci una nuova nota</h3>
 
                     <!-- Form per la creazione di una nuova nota -->
-                    <form id="new_note_form"  class="flex flex-col my-4">
+                    <form class="new_note_form flex flex-col my-4">
                         <input v-model="formStoreNotes.title" type="text" placeholder="Titolo" class="input input-bordered w-full" name="title" minlength="1" maxlength="50" required />
 
                         <textarea v-model="formStoreNotes.noteBody" class="textarea textarea-bordered my-4" placeholder="Testo della nota" name="noteBody" required></textarea>
